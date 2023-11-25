e164bfba4e5789ff4a003ffd15a45280af0c7eb6
ehddn <ehddn2202@gmail.com>
2023-11-25 16:10:53 +0900

    채팅방 만들기 프론트 페이지 생성 및 연동 중,,

--- project/backEnd/chat/urls.py
+++ project/backEnd/chat/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import RolePlayingRoomAPIView
 
 urlpatterns = [
-    path('new/', RolePlayingRoomAPIView.as_view()),
+    path('create/', RolePlayingRoomAPIView.as_view()),
 ]
